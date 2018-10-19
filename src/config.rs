24aa1ccd0fc8b8d24eb370293b22e9049b152468
Harry Stern <harry@harrystern.net>
2018-10-18 22:01:40 -0400

    Add explicit lifetime in Config::db
    - Removes a clone
    - Possibly not worth it because now there's a lifetime
    - The lifetime is technically valid: the DB needs the path from the
    config and takes a reference. However, I don't think it actually keeps
    the reference, it just uses it (inside rusqlite::Connection's open
    constructor)
    - Before, when I was making the db inline (i.e. I was calling
    make_sqlite_backend in the same scope as the config), I think rust was
    inferring the lifetime of the trait.
    - But I think that if I weren't using a trait in the first place, the
    lifetime wouldn't be necessary.

--- src/config.rs
+++ src/config.rs
@@ -14,13 +14,8 @@ pub struct Config {
 
 impl Config {
     /// Get a connection to the database at the location specified by the config file.
-    pub fn db(&self) -> Result<impl DBBackend, Error> {
-        // we clone here because the `impl DBBackend` return causes a lifetime error because I
-        // guess the compiler can't tell that we're actually using SqliteBackend, which doesn't
-        // keep a ref to the db_path. it's weird that it doesn't have that problem if i were to
-        // inline this in TKZCmd::dispatch though, so I feel like I could maybe add a lifetime
-        // somehow in the return type?
-        return make_sqlite_backend(self.db_path.clone())
+    pub fn db<'a>(&'a self) -> Result<impl DBBackend + 'a, Error> {
+        return make_sqlite_backend(&self.db_path)
             .map_err(|e| format_err!("Could not acquire database connection. {}", e));
     }
 
