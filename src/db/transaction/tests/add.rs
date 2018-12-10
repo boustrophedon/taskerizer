003ec9d18dd5e28f8ff17604cf455c940f047c6a
Harry Stern <harry@harrystern.net>
2018-12-09 20:34:19 -0500

    Add more tests for tx add task

--- src/db/transaction/tests/add.rs
+++ src/db/transaction/tests/add.rs
@@ -1,10 +1,18 @@
-use db::{DBTransaction, SqliteTransaction};
+use db::DBTransaction;
 use db::tests::open_test_db;
 
-use task::test_utils::example_task_1;
+use task::test_utils::{example_task_1, arb_task_list};
+
+// These tests don't really do anything. They just check that "if you add a valid task, the db does
+// not error."
+//
+// TODO add an invalid task (eg null bytes in string) and check that it errors?
+//
+// Alternatively, pass through the number of rows changed from the sql calls and check that it's
+// correct.
 
 #[test]
-fn test_db_tx_add_task_rollback() {
+fn test_tx_add_task_rollback() {
     let task = example_task_1();
 
     let mut db = open_test_db();
@@ -18,7 +26,7 @@ fn test_db_tx_add_task_rollback() {
 }
 
 #[test]
-fn test_db_tx_add_task_commit() {
+fn test_tx_add_task_commit() {
     let task = example_task_1();
 
     let mut db = open_test_db();
@@ -29,4 +37,34 @@ fn test_db_tx_add_task_commit() {
 
     let res = tx.commit();
     assert!(res.is_ok(), "Committing transaction failed: {}", res.unwrap_err());
+}
+
+proptest! {
+    #[test]
+    fn test_tx_add_task_rollback_arb(tasks in arb_task_list()) {
+        let mut db = open_test_db();
+        let tx = db.transaction().unwrap();
+
+        for task in tasks {
+            let res = tx.add_task(&task);
+            assert!(res.is_ok(), "Adding task failed: {}", res.unwrap_err());
+        }
+        let res = tx.rollback();
+        assert!(res.is_ok(), "Rolling back transaction failed: {}", res.unwrap_err());
+    }
+}
+
+proptest! {
+    #[test]
+    fn test_tx_add_task_commit_arb(tasks in arb_task_list()) {
+        let mut db = open_test_db();
+        let tx = db.transaction().unwrap();
+
+        for task in tasks {
+            let res = tx.add_task(&task);
+            assert!(res.is_ok(), "Adding task failed: {}", res.unwrap_err());
+        }
+        let res = tx.commit();
+        assert!(res.is_ok(), "Committing transaction failed: {}", res.unwrap_err());
+    }
 }
