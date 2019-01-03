761fd3f2f581515a4e61f4f29e67833f4278c65c
Harry Stern <harry@harrystern.net>
2019-01-03 00:16:39 -0500

    Add more tests for WeightedRandom strategy
    - Because they're random it's inherently flaky and I could probably do
    better

--- src/selection/tests/weighted_random.rs
+++ src/selection/tests/weighted_random.rs
@@ -1,4 +1,4 @@
-use crate::task::test_utils::{example_task_1, example_task_3, example_task_list, arb_task_list_bounded};
+use crate::task::test_utils::{example_task_1, example_task_2, example_task_3, arb_task_list};
 use crate::selection::{WeightedRandom, SelectionStrategy};
 
 // FIXME: compute actual 99.99% confidence intervals or whatever for the values and put
@@ -31,10 +31,10 @@ fn test_weighted_random_equal_priorities() {
     // "20" is basically arbitrary but it's unlikely to happen so it's good enough.
 
     assert!(counts[0] > 0, "First task was not chosen at all");
-    assert!(counts[0] > 20, "First task was not chosen enough");
+    assert!(counts[0] >= 20, "First task was not chosen enough");
 
     assert!(counts[1] > 0, "Second task was not chosen at all");
-    assert!(counts[1] > 20, "Second task was not chosen enough");
+    assert!(counts[1] >= 20, "Second task was not chosen enough");
 } 
 
 #[test]
@@ -52,11 +52,58 @@ fn test_weighted_random_equal_priorities_3() {
     // "10" is basically arbitrary but it's unlikely to happen so it's good enough.
 
     assert!(counts[0] > 0, "First task was not chosen at all");
-    assert!(counts[0] > 10, "First task was not chosen enough");
+    assert!(counts[0] >= 10, "First task was not chosen enough");
 
     assert!(counts[1] > 0, "Second task was not chosen at all");
-    assert!(counts[1] > 10, "Second task was not chosen enough");
+    assert!(counts[1] >= 10, "Second task was not chosen enough");
 
     assert!(counts[2] > 0, "Third task was not chosen at all");
-    assert!(counts[2] > 10, "Third task was not chosen enough");
-} 
+    assert!(counts[2] >= 10, "Third task was not chosen enough");
+}
+
+#[test]
+fn test_weighted_random_nonequal_priorities() {
+    let mut selector = WeightedRandom::new();
+
+    // task 2: priority 12
+    // task 3: priority 2
+    let tasks = vec![(0, example_task_2()), (1, example_task_3())];
+
+    let mut counts: [usize; 2] = [0,0];
+    for _ in 0..100 {
+        counts[selector.select_task(&tasks)] += 1;
+    }
+
+    // completely arbitrary heuristic: 1/4*n*p
+
+    // 100*12/14*1/4 = 21
+    assert!(counts[0] > 0, "Second task was not chosen at all");
+    assert!(counts[0] >= 20, "Second task was not chosen enough");
+
+    // 100*2/14*1/4 = 3 
+    assert!(counts[1] > 0, "Third task was not chosen at all");
+    assert!(counts[1] >= 2, "Third task was not chosen enough");
+}
+
+proptest! {
+    #[test]
+    fn test_weighted_random_nonequal_priorities_arb(tasks in arb_task_list()) {
+        let mut selector = WeightedRandom::new();
+
+        let num_items = tasks.len();
+        let tasks: Vec<_> = tasks.into_iter().enumerate().collect();
+        
+        let mut counts = vec![0; num_items];
+        for _ in 0..1000 {
+            counts[selector.select_task(&tasks)] += 1;
+        }
+
+        let p_denom = tasks.iter().fold(0f32, |acc, (_, task)| acc + task.priority() as f32);
+        for (i, task) in tasks {
+            // FIXME: again completely arbitrary
+            let heuristic = (0.25 * 1000.0 * (task.priority() as f32 / p_denom).floor()) as usize;
+            prop_assert!(counts[i] >= heuristic, "Task {} was not chosen enough: task {:?}, heuristic {}", i, task, heuristic);
+        }
+
+    }
+}
