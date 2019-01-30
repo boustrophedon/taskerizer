a6e7e9b53cad0098fd9a5cdcddb010c4166bdd67
Harry Stern <harry@harrystern.net>
2019-01-30 01:04:49 -0500

    Implement USetOp::apply_to_db for Add operation
    - We're basically just retesting DBBackend::add_task (which in turn is
    basically a wrapper around DBTransaction::add_task), but I think it's
    important for confidence in refactoring.

--- src/sync/mod.rs
+++ src/sync/mod.rs
@@ -1,10 +1,16 @@
-use uuid::Uuid;
 use crate::task::Task;
+use crate::db::DBBackend;
+
+use failure::Error;
+use uuid::Uuid;
 
 type ClientUuid = Uuid;
 
+#[cfg(test)]
+mod tests;
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
-enum USetOp {
+pub enum USetOp {
     Add (Task),
     Remove (Uuid),
 }
@@ -15,3 +21,36 @@ struct USetOpMsg {
     deliver_to: ClientUuid,
 }
 
+impl USetOp {
+    /// Apply the U-Set operation to the database. Returns a Result containing text to print to the
+    /// user, including but not limited to a notification that the current task was removed while
+    /// applying the operation.
+    pub fn apply_to_db(&self, tx: &impl DBBackend) -> Result<Vec<String>, Error> {
+        match self {
+            USetOp::Add(task) => USetOp::apply_add_to_db(tx, task).map(|_| Vec::new()),
+            USetOp::Remove(uuid) => {
+                // FIXME: there's probably a simpler way of doing this than the nested maps,
+                // probably using flat_map earlier.
+                USetOp::apply_remove_to_db(tx, uuid)
+                    .map(|opt_task| {
+                        opt_task.iter().flat_map(|task| {
+                            vec![format!("Current task removed during sync: {}", task.task())]
+                        }).collect()
+                    })
+            }
+        }
+    }
+
+    /// Apply a U-Set add operation to the database. This will error if a task with the same UUID
+    /// exists in the database.
+    fn apply_add_to_db(tx: &impl DBBackend, task: &Task) -> Result<(), Error> {
+        tx.add_task(task)
+    }
+
+    /// Apply a U-Set remove operation to the database. If the task to be removed was the current task, it
+    /// is returned so the user can be notified. If there is no task in the database with the given
+    /// UUID, nothing happens.
+    fn apply_remove_to_db(tx: &impl DBBackend, uuid: &Uuid) -> Result<Option<Task>, Error> {
+        unimplemented!()
+    }
+}
---
+++ src/sync/tests/add.rs
@@ -0,0 +1,91 @@
+use std::collections::HashSet;
+
+use crate::db::DBBackend;
+use crate::task::Task;
+use crate::sync::USetOp;
+
+use crate::db::tests::open_test_db;
+use crate::task::test_utils::{example_task_1, example_task_break_1, example_task_2};
+use super::uset_add_list_arb;
+
+#[test]
+/// Add via uset, check task is in list
+fn test_uset_add_task_1() {
+    let mut db = open_test_db();
+    let tx = db.transaction().unwrap();
+
+    let task = example_task_1();
+
+    // add example task
+    let op = USetOp::Add(task.clone());
+    let res = op.apply_to_db(&tx);
+    assert!(res.is_ok(), "Could not apply add operation to db: {}", res.unwrap_err());
+
+    // read tasks from db and check it's there
+    let db_tasks = tx.fetch_all_tasks().expect("Couldn't fetch tasks");
+    assert_eq!(db_tasks[0], task);
+}
+
+#[test]
+/// Add via uset, check task is in list
+fn test_uset_add_break_1() {
+    let mut db = open_test_db();
+    let tx = db.transaction().unwrap();
+
+    let task = example_task_break_1();
+
+    // add example task
+    let op = USetOp::Add(task.clone());
+    let res = op.apply_to_db(&tx);
+    assert!(res.is_ok(), "Could not apply add operation to db: {}", res.unwrap_err());
+
+    // read tasks from db and check it's there
+    let db_tasks = tx.fetch_all_tasks().expect("Could not get tasks");
+    assert_eq!(db_tasks[0], task);
+}
+
+#[test]
+/// Add test "locally", then add same task with same uuid via USetOp. Check for error.
+fn test_uset_add_error_dup_uuid() {
+    let mut db = open_test_db();
+    let tx = db.transaction().unwrap();
+    let task = example_task_2();
+
+    // add example task;
+    tx.add_task(&task).expect("Failed adding test");
+
+    let op = USetOp::Add(task.clone());
+    let res = op.apply_to_db(&tx);
+    assert!(res.is_err(), "No error adding task with duplicate uuid to db");
+
+    let err = res.unwrap_err();
+    assert!(err.to_string().contains("UNIQUE constraint failed: tasks.uuid"),
+        "Incorrect error message when inserting duplicate task: got {}", err);
+}
+
+proptest! {
+    #[test]
+    /// Add tasks via USet, check they are all there.
+    fn test_uset_add_arb(ops in uset_add_list_arb()) {
+        let mut db = open_test_db();
+        let tx = db.transaction().unwrap();
+
+        // apply all add ops
+        for op in &ops {
+            let res = op.apply_to_db(&tx);
+            prop_assert!(res.is_ok(), "Could not apply add operation to db: {}", res.unwrap_err());
+        }
+
+        let db_tasks = tx.fetch_all_tasks().expect("Couldn't fetch tasks");
+        // check number of tasks in db is equal to number of add ops performed
+        prop_assert_eq!(db_tasks.len(), ops.len());
+
+        // put the ops into a hashset for faster checking - there shouldn't be any dups except for
+        // small probability of uuid collision
+        let tasks_set: HashSet<Task> = ops.into_iter().map(|op| op.unwrap_add()).collect();
+        for db_task in &db_tasks {
+            // check db task in tasks from add ops
+            prop_assert!(tasks_set.contains(db_task));
+        }
+    }
+}
